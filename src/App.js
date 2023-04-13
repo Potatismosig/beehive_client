0405aa8eb1a5f7c8c4aa8dd75e9995aeff9d96d4
Bagher hosseini <bagherhosseini03@gmail.com>
2023-04-13 10:46:25 +0200

    created routes setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Register from './pages/register';
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <Register></Register>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
