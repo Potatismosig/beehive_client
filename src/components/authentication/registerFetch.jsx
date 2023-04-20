0e2ae88e870e2e0ced095747493a26f2f66da982
Bagher hosseini <bagherhosseini03@gmail.com>
2023-04-20 10:14:13 +0200

    changed fetch url

--- src/components/authentication/registerFetch.jsx
+++ src/components/authentication/registerFetch.jsx
@@ -1,5 +1,5 @@
 export default function registerFetch(username, password) {
-    return fetch('http://localhost:5050/register', {
+    return fetch('http://localhost:5050/auth/register', {
         method: 'POST',
         body: JSON.stringify({ username, password }),
         headers: {
