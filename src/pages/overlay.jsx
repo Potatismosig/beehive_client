6b688d1d635874717e68933842d97a8f95944b76
gufr2201 <gfranchell@gmail.com>
2023-04-25 17:21:19 +0200

    Updated strings to match our page better

--- src/pages/overlay.jsx
+++ src/pages/overlay.jsx
@@ -7,10 +7,9 @@ export default function Overlay({ onSignUpClick, onSignInClick }) {
             <div className="overlay">
                 <div className="overlay-panel overlay-left">
                 <img src="https://cdn.discordapp.com/attachments/1074053506954186792/1098585647717027890/output_1.png" alt="" />
-                    <h1>Welcome Back!</h1>
+                    <h1>Welcome to BeeHive!</h1>
                     <p>
-                        To keep connected with us please login with your personal
-                        info
+                        To connect with your friends please sign in
                     </p>
                     <button className="ghost" id="signIn" onClick={onSignInClick}>
                         Sign In
@@ -18,9 +17,9 @@ export default function Overlay({ onSignUpClick, onSignInClick }) {
                 </div>
                 <div className="overlay-panel overlay-right">
                 <img className='rightImg' src="https://cdn.discordapp.com/attachments/1074053506954186792/1098575736094343188/Lovepik_com-402425950-3d-cartoon-style-plush-yellow-flower-model-element.png" alt="" />
-                    <h1>Hello, Friend!</h1>
+                    <h1>Welcome to BeeHive!</h1>
                     <p>
-                        Enter your personal details and start journey with us
+                        Create your account and start your journey with us
                     </p>
                     <button className="ghost" id="signUp" onClick={onSignUpClick}>
                         Sign Up
