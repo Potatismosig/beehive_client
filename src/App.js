ec39d50b4022ecf7b06ea17b850054e6db756bf9
gufr2201 <gfranchell@gmail.com>
2023-04-25 17:23:27 +0200

    imports header containing navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import {Auth} from './pages/index';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Header from './components/pageComponents/header';
 function App() {
   return (
     <main>
       <BrowserRouter>
+    <Header />
         <Routes>
           <Route path="/" element={<Auth />} />
         </Routes>
